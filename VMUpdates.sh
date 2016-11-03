2a0cef65d247899beac937de8c8233385ea2b5fc
leemoores2 <lee.moores@rebasoft.net>
2016-11-03 16:47:08 +0000

    Added VMUpdate.sh via upload

---
+++ VMUpdates.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+
+
+ip_address=( 172.16.100.22 172.16.100.23 172.16.100.24 172.16.100.25 172.16.100.26 172.16.100.27 172.16.100.28 172.16.100.29 );
+
+
+for ip in ${ip_address[@]}
+
+
+
+do
+
+
+
+ssh -t root@$ip /bin/sh <<\EOF
+
+
+
+yum -y update
+
+##### Update Webmin #####
+
+if [ ! -f /opt/software/webmin-1.820-1.noarch.rpm ]; then
+
+wget http://prdownloads.sourceforge.net/webadmin/webmin-1.820-1.noarch.rpm -P /opt/software
+rpm -Uvh /opt/software/webmin-1.820-1.noarch.rpm
+wget --no-cache http://builds.rebasoft.net/builder/rebasoft_webmin_new.tgz
+
+##### Update JAVA #####
+
+if [ ! -f /usr/java/jre1.8.0_111 ]; then
+
+rm -rf /opt/java
+wget http://builds.rebasoft.net/builder/jre/jre-8u111-linux-x64.rpm -P /opt/software
+rpm ivf /opt/software/jre-8u111-linux-x64.rpm
+ln -s /usr/java/jre1.8.0_111/ /opt/java
+
+fi
+
+EOF
+
+
+done
