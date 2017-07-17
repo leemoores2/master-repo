9960f491936c6fec118bbea693b5d59874e15407
leemoores2 <lee.moores@rebasoft.net>
2017-07-17 13:00:07 +0100

    Update CI.sh

--- CI.sh
+++ CI.sh
@@ -8,7 +8,7 @@ build_no=$5 # example: 43
 prod=$6
 
 if [[ $prod == "CORE" ]]; then
-wget http://172.16.100.16:8085/browse/SAB-AC$1$4-$5/artifact/shared/tar.gz/rebasoft-auditor-core-$2$3$4-BUILD$5.tar.gz  -P /opt/software/
+wget http://172.16.100.16:8085/browse/SAB-AC$1$3-$4/artifact/shared/tar.gz/rebasoft-auditor-core-$2$3$4-BUILD$5.tar.gz  -P /opt/software/
 pid=`cat /opt/Rebasoft/MACAuditor/saber.pid`
 kill -9 ${pid}
 rm -rf /opt/Rebasoft/MACAuditor
@@ -19,7 +19,7 @@ service macauditord start
 fi
 
 if [[ $prod == "APP" ]]; then
-wget http://172.16.100.16:8085/browse/SCIM-AA$1$4-$5/artifact/shared/tar.gz/rebasoft-application-auditor-$2$3$4-$5.tar.gz  -P /opt/software/
+wget http://172.16.100.16:8085/browse/SCIM-AA$1$3-$4/artifact/shared/tar.gz/rebasoft-application-auditor-$2$3$4-$5.tar.gz  -P /opt/software/
 pid=`cat /opt/Rebasoft/ApplicationAuditor/scimitar.pid`
 kill -9 ${pid}
 rm -rf /opt/Rebasoft/ApplicationAuditor
